cc2ac60d296c9bf14a4106386824363537651053
Lawrence <media@absalom.biz>
2015-10-19 14:57:16 +1000

    More strings

--- ext_sass.cpp
+++ ext_sass.cpp
@@ -78,13 +78,13 @@ static void set_options(ObjectData* obj, struct Sass_Context *ctx) {
   sass_option_set_source_map_contents(opts, obj->o_get("map_contents", true, s_Sass).toBoolean());
   String mapLink = obj->o_get("map_path", true, s_Sass);
   if (!mapLink.empty()) {
-  sass_option_set_source_map_file(opts, obj->o_get(mapLink, true, s_Sass));
+  sass_option_set_source_map_file(opts, obj->o_get(mapLink, true, s_Sass).toStrRef());
   sass_option_set_omit_source_map_url(opts, false);
   sass_option_set_source_map_contents(opts, true);
   }
   String mapRoot = obj->o_get("map_root", true, s_Sass);
   if (!mapRoot.empty()) {
-  sass_option_set_source_map_root(opts, obj->o_get(mapRoot, true, s_Sass));
+  sass_option_set_source_map_root(opts, obj->o_get(mapRoot, true, s_Sass).toStrRef());
   }
 
 }
