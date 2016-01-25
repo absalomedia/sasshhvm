05f048e886d94de1a77fe1057723a327a05fa4cb
Lawrence <media@absalom.biz>
2016-01-25 11:27:57 +1000

    Libsass 3.3.3 update

--- ext_sass.cpp
+++ ext_sass.cpp
@@ -148,7 +148,7 @@ static String HHVM_STATIC_METHOD(Sass, getLibraryVersion) {
 
 static class SassExtension : public Extension {
  public:
-  SassExtension() : Extension("sass", "0.4.2") {}
+  SassExtension() : Extension("sass", "0.4.3") {}
   virtual void moduleInit() {
     HHVM_ME(Sass, compile);
     HHVM_ME(Sass, compileFileNative);
